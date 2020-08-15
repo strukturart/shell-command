3350b1c023efb7065ba5326b0d1289612d83e54f
John-David Deubl <print@strukturart.com>
2020-08-15 12:15:42 +0200

    permission

--- README.md
+++ README.md
@@ -40,3 +40,25 @@ download .JPG files from a Yi4k+ Camera
 
 ]
 ```
+
+
+## Donation
+If you use the app often, please donate an amount to me.
+<br>
+<table class="border-0"> 
+  <tr class="border-0" >
+    <td valign="top" class="border-0">
+        <div>
+            <a href="https://paypal.me/strukturart?locale.x=de_DE" target="_blank">
+                <img src="/images/paypal.png" width="120px">
+            </a>
+        </div>
+    </td>
+    <td valign="top" class="border-0">
+        <div>
+            <div>Bitcoin</div>
+            <img src="/images/bitcoin_rcv.png" width="120px">
+        </div>
+    </td>
+  </tr>
+ </table>
--- application/assets/js/script.js
+++ application/assets/js/script.js
@@ -1,287 +1,241 @@
+$(document).ready(function() {
 
-$(document).ready(function() 
- {
 
+    var i = 0;
+    var z = -1;
+    var finderNav_tabindex = -1;
 
-	//Global Vars
-	var i = 0;
-	var z = -1;
-	var finderNav_tabindex = -1;
-	var debug = false;
 
+    /////////////////////////
+    function finder() {
 
-	/////////////////////////
-	function finder()
-	{
 
+        var finder = new Applait.Finder({
+            type: "sdcard",
+            debugMode: true
+        });
 
-	var finder = new Applait.Finder({ type: "sdcard", debugMode: true });
 
+        finder.on("empty", function(needle) {
+            alert("no sdcard found");
+            return;
+        });
 
-		finder.on("empty", function (needle) 
-		{
-			alert("no sdcard found");
-			return;
-		});
+        finder.search("cmd.json");
 
-		finder.search("cmd.json");
 
 
+        finder.on("fileFound", function(file, fileinfo, storageName) {
 
-		finder.on("fileFound", function (file, fileinfo, storageName) 
-		{
+            var reader = new FileReader()
 
-			var reader = new FileReader()
 
+            reader.onerror = function(event) {
+                alert('shit happens')
+                reader.abort();
+            };
 
-			reader.onerror = function(event) 
-					{
-						alert('shit happens')
-						reader.abort();
-					};
+            reader.onloadend = function(event) {
 
-					reader.onloadend = function (event) 
-					{
+                search_result = event.target.result
 
-							search_result = event.target.result
-							
-							//check if json valid
-							var printError = function(error, explicit) {
-							console.log("[${explicit ? 'EXPLICIT' : 'INEXPLICIT'}] ${error.name}: ${error.message}");
-							}
+                //check if json valid
+                var printError = function(error, explicit) {
+                    console.log("[${explicit ? 'EXPLICIT' : 'INEXPLICIT'}] ${error.name}: ${error.message}");
+                }
 
-							try {
-							   
-							} catch (e) {
-							    if (e instanceof SyntaxError) {
-							        alert("Json file is not valid");
-							        return;
-							    } else {
-							        
-							    }
+                try {
 
-							}
-									var data = JSON.parse(search_result);
-									$.each(data, function(i, item) {
-									finderNav_tabindex++;
-									$("div#app-list").append('<div class="items" tabindex="'+finderNav_tabindex+'"  data-cmd="'+item.cmd+'">'+item.cmd_name+'</div>');
-									
-									});
+                } catch (e) {
+                    if (e instanceof SyntaxError) {
+                        alert("Json file is not valid");
+                        return;
+                    } else {
 
-					$('div#finder').find('div.items[tabindex=0]').focus();
+                    }
 
-					};
-					reader.readAsText(file)
-					});
+                }
+                var data = JSON.parse(search_result);
+                $.each(data, function(i, item) {
+                    finderNav_tabindex++;
+                    $("div#app-list").append('<div class="items" tabindex="' + finderNav_tabindex + '"  data-cmd="' + item.cmd + '">' + item.cmd_name + '</div>');
 
+                });
 
-	}	
+                $('div#finder').find('div.items[tabindex=0]').focus();
 
+            };
+            reader.readAsText(file)
+        });
 
-finder()
 
+    }
 
-  
-////////////////////////
-//NAVIGATION
-/////////////////////////
 
+    finder()
 
 
-	function nav (move) {
-	var items = document.querySelectorAll('.items');
-		if(move == "+1" && i < finderNav_tabindex)
-		{
-			i++
 
-			if(i <= finderNav_tabindex)
-			{
-				var items = document.querySelectorAll('.items');
-				var targetElement = items[i];
-				targetElement.focus();
+    ////////////////////////
+    //NAVIGATION
+    /////////////////////////
 
-			}
-		}
 
-		if(move == "-1" &&  i > 0)
-		{
-			i--
-			if(i >= 0)
-			{
-				var items = document.querySelectorAll('.items');
-				var targetElement = items[i];
-				targetElement.focus();
 
-			}
-		}
+    function nav(move) {
+        var items = document.querySelectorAll('.items');
+        if (move == "+1" && i < finderNav_tabindex) {
+            i++
 
-	}
+            if (i <= finderNav_tabindex) {
+                var items = document.querySelectorAll('.items');
+                var targetElement = items[i];
+                targetElement.focus();
 
+            }
+        }
 
+        if (move == "-1" && i > 0) {
+            i--
+            if (i >= 0) {
+                var items = document.querySelectorAll('.items');
+                var targetElement = items[i];
+                targetElement.focus();
 
-/////////////////
-//VIBRATION/////
-///////////////
+            }
+        }
 
+    }
 
-var vibrateInterval;
 
-// Starts vibration at passed in level
-function startVibrate(duration) {
-    navigator.vibrate(duration);
-}
 
-// Stops vibration
-function stopVibrate() {
-    // Clear interval and stop persistent vibrating
-    if(vibrateInterval) clearInterval(vibrateInterval);
-    navigator.vibrate(0);
-}
+    /////////////////
+    //VIBRATION/////
+    ///////////////
 
-// Start persistent vibration at given duration and interval
-// Assumes a number value is given
-function startPersistentVibrate(duration, interval) {
-    vibrateInterval = setInterval(function() {
-        startVibrate(duration);
-    }, interval);
-}
 
+    var vibrateInterval;
 
+    // Starts vibration at passed in level
+    function startVibrate(duration) {
+        navigator.vibrate(duration);
+    }
 
+    // Stops vibration
+    function stopVibrate() {
+        // Clear interval and stop persistent vibrating
+        if (vibrateInterval) clearInterval(vibrateInterval);
+        navigator.vibrate(0);
+    }
 
-//////////////////
-//Execute command
-//////////////////
+    // Start persistent vibration at given duration and interval
+    // Assumes a number value is given
+    function startPersistentVibrate(duration, interval) {
+        vibrateInterval = setInterval(function() {
+            startVibrate(duration);
+        }, interval);
+    }
 
-function exec_cmd()
-{
 
 
 
-	var selected_button = $(":focus")[0];
-	var cmd = selected_button.getAttribute('data-cmd');
-	cmd = "echo `date +%d-%m-%y`&&"+cmd+" > /storage/sdcard/cmd/cmd_log.txt 2>&1";
+    //////////////////
+    //Execute command
+    //////////////////
 
-	$("div#output").text("command is executed "+cmd)
+    function exec_cmd() {
 
 
 
-	
+        var selected_button = $(":focus")[0];
+        var cmd = selected_button.getAttribute('data-cmd');
+        cmd = "echo `date +%d-%m-%y`&&" + cmd + " > /storage/sdcard/cmd/cmd_log.txt 2>&1";
 
-		var extension = navigator.kaiosExtension || navigator.engmodeExtension;
-		if(extension)
-		{
-			var executor = extension.startUniversalCommand(cmd, true); 
-			executor.onsuccess = function(e)
-			{
-				setTimeout(function () 
-				{
-				$("div#output").text("success")
-				startVibrate([100,100,200,100,300]);
+        $("div#output").text("command is executed " + cmd)
 
-				}, 4000);
-			};
+        var extension = navigator.kaiosExtension || navigator.engmodeExtension;
+        if (extension) {
+            var executor = extension.startUniversalCommand(cmd, true);
+            executor.onsuccess = function(e) {
+                setTimeout(function() {
+                    $("div#output").text("success")
+                    startVibrate([100, 100, 200, 100, 300]);
 
-			executor.onerror = function(e)
-			{
-				$("div#output").text("error")
-			};
-		}
-		else 
-		{
-			$("div#output").text('no extension object available');
-		}
+                }, 4000);
+            };
 
+            executor.onerror = function(e) {
+                $("div#output").text("error")
+            };
+        } else {
+            alert('no extension object available');
+        }
 
 
-		}
 
+    }
 
 
-function show_man()
-{
-	$("div#man-page").css('display','block')
-	window_status = true;
-}
 
-function close_man()
-{
-	$("div#man-page").css('display','none')
-	window_status = false;
-}
+    function show_man() {
+        $("div#man-page").css('display', 'block')
+        window_status = true;
+    }
 
+    function close_man() {
+        $("div#man-page").css('display', 'none')
+        window_status = false;
+    }
 
 
 
 
 
-	//////////////////////////
-	////KEYPAD TRIGGER////////////
-	/////////////////////////
 
+    //////////////////////////
+    ////KEYPAD TRIGGER////////////
+    /////////////////////////
 
 
-	function handleKeyDown(evt) {
 
+    function handleKeyDown(evt) {
 
-			switch (evt.key) {
 
+        switch (evt.key) {
 
-	        case 'Enter':
-	        exec_cmd();
-	        break;
 
+            case 'Enter':
+                exec_cmd();
+                break;
 
-			case 'ArrowDown':
-				nav("+1")
-			break; 
 
+            case 'ArrowDown':
+                nav("+1")
+                break;
 
-			case 'ArrowUp':
-				nav("-1")
-			break; 
 
-			case '0':
-				show_man();
-			break; 
+            case 'ArrowUp':
+                nav("-1")
+                break;
 
-			case 'SoftLeft':
-				close_man();
-			break; 
- 
+            case '0':
+                show_man();
+                break;
 
-		}
+            case 'SoftLeft':
+                close_man();
+                break;
 
-	};
 
+        }
 
+    };
 
-	document.addEventListener('keydown', handleKeyDown);
 
 
-	//////////////////////////
-	////BUG OUTPUT////////////
-	/////////////////////////
+    document.addEventListener('keydown', handleKeyDown);
 
-if(debug == true)
-{
-	$(window).on("error", function(evt) {
 
-	console.log("jQuery error event:", evt);
-	var e = evt.originalEvent; // get the javascript event
-	console.log("original event:", e);
-	if (e.message) { 
-	    alert("Error:\n\t" + e.message + "\nLine:\n\t" + e.lineno + "\nFile:\n\t" + e.filename);
-	} else {
-	    alert("Error:\n\t" + e.type + "\nElement:\n\t" + (e.srcElement || e.target));
-	}
-	});
-}
+
 
 });
-
-
-
-
-
-
